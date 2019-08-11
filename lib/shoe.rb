7a2a75455c3014dc3276819c774eff503a5f5429
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-11 17:15:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def cobble
+    
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
